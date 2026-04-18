9302954f0cca82df5da5dfde362f156e3778e18a
talkal123 <talkal153@gmail.com>
2026-04-18 18:52:19 +0300

    New commit to merge client and server

--- server/server.js
+++ server/server.js
@@ -23,7 +23,11 @@ const bcrypt = require('bcrypt');
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://my-tor-seven.vercel.app', // הכתובת המדויקת של הפרונטאנד שלך
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // חשוב אם אתה משתמש ב-Cookies או Sessions
+}));
 
 
 
